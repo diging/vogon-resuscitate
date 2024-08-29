30a28bebb9166399b9e20b4fdc10ac1754970a46
Julia Damerow <jdamerow@asu.edu>
2024-08-29 14:41:33 -0400

    added noinput to collectstatic

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@
 ./vogon/.env-app
 python -m pip install -r requirements.txt
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
