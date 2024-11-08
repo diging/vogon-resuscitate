36b90e8ecc43083ed674483c74ed744fcba242d3
Girik1105 <girik1105@gmail.com>
2024-11-08 12:32:34 -0700

    [VOGRE-7] Removed duplicate graph data function

--- annotations/utils.py
+++ annotations/utils.py
@@ -82,66 +82,6 @@ def get_relation_node(user):
         }
     }
 
-def generate_graph_data(relationsets, user):
-    nodes = {}
-    edges = []
-    node_counter = 0
-
-    def get_node_id():
-        nonlocal node_counter
-        node_id = str(node_counter)
-        node_counter += 1
-        return node_id
-
-    for rs in relationsets:
-        for relation in rs.constituents.all():
-            subject = getattr(relation.source_content_object, 'interpretation', None)
-            obj = getattr(relation.object_content_object, 'interpretation', None)
-            predicate = getattr(relation.predicate, 'interpretation', None)
-
-            if subject and subject.uri not in nodes:
-                subject_id = get_node_id()
-                nodes[subject_id] = build_concept_node(subject, user)
-
-            if obj and obj.uri not in nodes:
-                obj_id = get_node_id()
-                nodes[obj_id] = build_concept_node(obj, user)
-
-            if predicate and predicate.uri not in nodes:
-                predicate_id = get_node_id()
-                nodes[predicate_id] = build_concept_node(predicate, user)
-
-            relation_id = get_node_id()
-            nodes[relation_id] = get_relation_node(user)
-
-            if subject and predicate:
-                edges.append({"source": subject_id, "relation": "subject", "target": predicate_id})
-
-            if predicate and obj:
-                edges.append({"source": predicate_id, "relation": "predicate", "target": obj_id})
-
-            edges.append({"source": relation_id, "relation": "object", "target": obj_id})
-
-    return {
-        "graph": {
-            "metadata": {
-                "defaultMapping": {
-                    "subject": {"type": "REF", "reference": "0"},
-                    "predicate": {"type": "URI", "uri": "", "label": ""},
-                    "object": {"type": "REF", "reference": "3"}
-                },
-                "context": {
-                    "creator": user.username,
-                    "creationTime": timezone.now().strftime('%Y-%m-%d'),
-                    "creationPlace": "phoenix",
-                    "sourceUri": ""
-                }
-            },
-            "nodes": nodes,
-            "edges": edges
-        }
-    }
-
 def generate_graph_data(relationset, user):
     nodes = {}
     edges = []
