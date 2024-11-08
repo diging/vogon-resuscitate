17d73baa9554bf3dbde45bea9e0a67f72c8afa08
Rajvi Patel <rajvi2232000@gmail.com>
2024-11-08 16:32:44 -0700

    [VOGRE-19] Fix appellation list

--- annotations/static/annotations/js/annotators/appellationlist.js
+++ annotations/static/annotations/js/annotators/appellationlist.js
@@ -217,6 +217,7 @@ AppellationList = {
             // Replace an array prop wholesale doesn't seem to trigger a
             //  DOM update in the v-for binding, but a push() does; so we'll
             //  just push the appellations that aren't already in the array.
+            this.current_appellations = [];
             var current_ids = this.current_appellations.map(function (elem) {
                 return elem.id;
             });
