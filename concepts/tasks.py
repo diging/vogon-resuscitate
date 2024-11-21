9ce5df5847b4963494904156f8816510897b9313
Girik1105 <girik1105@gmail.com>
2024-11-21 10:02:12 -0700

    [VOGRE-7] Removed celery instances in concepts app

--- concepts/tasks.py
+++ concepts/tasks.py
@@ -1,10 +1,10 @@
 
 
 from django.conf import settings
-import logging
-logging.basicConfig()
-logger = logging.getLogger(__name__)
-logger.setLevel(settings.LOGLEVEL)
+# import logging
+# logging.basicConfig()
+# logger = logging.getLogger(__name__)
+# logger.setLevel(settings.LOGLEVEL)
 
 import requests
 
@@ -14,23 +14,24 @@ from concepts.lifecycle import *
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from celery import shared_task
+# from celery import shared_task
 
 
-@shared_task
+# @shared_task
 def resolve_concept(instance_id):
-    """
-    Since resolving concepts can involve several API calls, we handle it
-    asynchronously.
-    """
-    try:
-        manager = ConceptLifecycle(Concept.objects.get(pk=instance_id))
-    except Concept.DoesNotExist:
-        return
+    # """
+    # Since resolving concepts can involve several API calls, we handle it
+    # asynchronously.
+    # """
+    # try:
+    #     manager = ConceptLifecycle(Concept.objects.get(pk=instance_id))
+    # except Concept.DoesNotExist:
+    #     return
 
-    try:
-        manager.resolve()
-    except ConceptLifecycleException as E:
-        logger.debug("Resolve concept failed: %s" % str(E))
-        return 
-    logger.debug("Resolved concept %s" % manager.instance.uri)
+    # try:
+    #     manager.resolve()
+    # except ConceptLifecycleException as E:
+    #     logger.debug("Resolve concept failed: %s" % str(E))
+    #     return 
+    # logger.debug("Resolved concept %s" % manager.instance.uri)
+    pass
