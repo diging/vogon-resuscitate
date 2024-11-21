de7fd862860616721ace1435c8ee8be3162b351c
Rajvi Patel <rajvi2232000@gmail.com>
2024-11-21 11:50:56 -0700

    [VOGRE-16] Fix bash file for prod and gunicorn

--- vogon-gunicorn.sh
+++ vogon-gunicorn.sh
@@ -2,23 +2,17 @@
 
 NAME="vogon"                                  # Name of the application
 DJANGODIR=/vogon                              # Django project directory
-SOCKFILE=/usr/src/app/run/jars.sock           # Unix socket for communication
 NUM_WORKERS=3                                 # Number of Gunicorn workers
-DJANGO_SETTINGS_MODULE=jars.settings          # Django settings module
-DJANGO_WSGI_MODULE=jars.wsgi                  # WSGI module name
-LOGFILE=/var/log/$NAME/gunicorn.log           # Log file path
+DJANGO_SETTINGS_MODULE=vogon.settings          # Django settings module
+DJANGO_WSGI_MODULE=vogon.wsgi                  # WSGI module name
 
 echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
-source .env
+source .env_app
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
-
-# Create the run directory if it doesn't exist
-RUNDIR=$(dirname $SOCKFILE)
-test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
