4336950954dee04ca9746e8e9d80b58be210dc69
Girik1105 <girik1105@gmail.com>
2024-12-11 11:07:54 -0700

    [VOGRE-7] Got rid of middleware

--- vogon/settings.py
+++ vogon/settings.py
@@ -67,7 +67,6 @@ MIDDLEWARE = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    # 'annotations.middleware.CheckRelationSetStatusMiddleware',
 )
 
 ROOT_URLCONF = 'vogon.urls'
