3589b00799c5ffbf521a8cfe791fe152a7fdd5ef
Rajvi Patel <rajvi2232000@gmail.com>
2025-01-07 16:33:14 -0700

    [VOGRE-16] Update command in startup file

--- startup-prod.sh
+++ startup-prod.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-BASH_ENV=.env_app
+source .env_app
 mkdir -p /vogon/logs
 python -m pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
-exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
+sudo systemctl start supervisor
