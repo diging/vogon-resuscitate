bc98c329c514c1dd4e7bd88f56de3dded420e2f7
Girik1105 <girik1105@gmail.com>
2025-04-01 11:51:21 -0700

    [VOGRE-54] Reset store and resources

--- annotations/static/annotations/js/annotators/resources.js
+++ annotations/static/annotations/js/annotators/resources.js
@@ -1,42 +1,37 @@
+
 /******************************************************************************
   *         Resources!
   *****************************************************************************/
 
 // Function to get the value of a cookie by its name
 function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-      const cookies = document.cookie.split(';');
-      for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim();
-        // Check if this cookie's name matches the one we're looking for
-        if (cookie.substring(0, name.length + 1) === (name + '=')) {
-          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-          break;
-        }
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Check if this cookie's name matches the one we're looking for
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
       }
     }
-    return cookieValue;
   }
+  return cookieValue;
+}
 
 Vue.http.headers.common['X-CSRFToken'] = getCookie('csrftoken');
 
-const REST_URL = BASE_URL + '/rest/';
 
-var Appellation = Vue.resource(REST_URL + 'appellation{/id}', {}, {
-    delete: {
-        method: 'DELETE',
-        url: REST_URL + 'appellation{/id}'
-    }
-});
+var Appellation = Vue.resource(BASE_URL + '/rest/appellation{/id}');
 var DateAppellation = Vue.resource(BASE_URL + '/rest/dateappellation{/id}');
 var Relation = Vue.resource(BASE_URL + '/rest/relationset{/id}');
 var Concept = Vue.resource(BASE_URL + '/rest/concept{/id}', {}, {
-    search: {method: 'GET', url: BASE_URL + '/rest/concept/search'}
+  search: {method: 'GET', url: BASE_URL + '/rest/concept/search'}
 });
 var RelationTemplateResource = Vue.resource(BASE_URL + '/relationtemplate{/id}/', {}, {
-    create: {method: 'POST', url: BASE_URL + '/relationtemplate{/id}/create/'},
-    text: {method: 'POST', url: BASE_URL + '/relationtemplate{/id}/create/text/'},
-    get_single_relation: {method: 'GET', url: BASE_URL + '/rest/templates/get_single_relation/'},
+  create: {method: 'POST', url: BASE_URL + '/relationtemplate{/id}/create/'},
+  text: {method: 'POST', url: BASE_URL + '/relationtemplate{/id}/create/text/'},
+  get_single_relation: {method: 'GET', url: BASE_URL + '/rest/templates/get_single_relation/'},
 });
 var ConceptType = Vue.resource(BASE_URL + '/rest/type{/id}');
