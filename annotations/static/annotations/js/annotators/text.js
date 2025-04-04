06c96c1b2abbb71064760be0ec28a2e4ac9169ad
Rajvi Patel <rajvi2232000@gmail.com>
2025-04-04 08:35:07 -0700

    story/VOGRE-36 (#36)
    * [VOGRE-32] Fix bugs in UI for new concept label
    
    * [VOGRE-32] Debug concept lifecycle
    
    * [VOGRE-32] Update conceptpower api
    
    * [VOGRE-32] Remove approve state and update post request for add
    
    * [VOGRE-32] Addfunction to check equal uris
    
    * [VOGRE-32] Remove conceptpower api calls
    
    * [VOGRE-32] Code cleanup and fix concept cycle
    
    * [VOGRE-32] Remove debugging statements
    
    * [VOGRE-32] Remove unnecessary  comments
    
    * [VOGRE-36] Add user field for concept model
    
    * [VOGRE-36] Add user when new concept is created
    
    * [VOGRE-32] Create conceptpower class
    
    * [VOGRE-32] Update conceptpower class and code cleanup
    
    * [VOGRE-36] Remove user.name in js
    
    * [VOGRE-36] Revert username
    
    * [VOGRE-32] Fix conceptpower search function
    
    * [VOGRE-36] Remove decorator from concept actions
    
    * [VOGRE-36] Remove debug statements
    
    * [VOGRE-32] Code cleanup
    
    * [VOGRE-32] Remove get_type class method
    
    * [VOGRE-32] Update conceptpower class
    
    * [VOGRE-32] Fix spacing
    
    * [VOGRE-32] Add comment for parsing function
    
    * [VOGRE-46] Add comment for conceptpower endpoint (#45)
    
    * [VOGRE-32] Handle empty response for concept search
    
    * [VOGRE-32] Address review comments
    
    * [VOGRE-32] Fix conceptpower namespace
    
    * [VOGRE-32] Fix import error for conceptpower
    
    * [VOGRE-36] Add login_Required decorator for concept actions
    
    * [VOGRE-32] Remove approve from filters and tests
    
    * [VOGRE-32] Update decorator for vogon
    
    * [VOGRE-32] Remove admin actions for concepts
    
    * [VOGRE-32] Revert concept state change
    
    * [VOGRE-36] Revert new line changes
    
    * [VOGRE-32] Make migrations
    
    * Bug/VOGRE-48 (#50)
    
    * [VOGRE-48] List concepts while creating templates
    
    * [VOGRE-48] Fix predicate concept and object concept
    
    * [VOGRE-48] Fix AutocompleteWidget and to_python function
    
    * [VOGRE-48] Revert Relationtemplate edit changes
    
    * [VOGRE-48] Format forms.py
    
    * [VOGRE-48] Remove conceptcycle import
    
    * [VOGRE-48] Update conceptpower class
    
    * Revert "[VOGRE-48] Update conceptpower class"
    
    This reverts commit 33f93c4d7a754f4b08e63f65369c5e0452d982b6.
    
    * [VOGRE-48] Add conceptpower class
    
    * [VOGRE-32] Update conceptpower connection error
    
    * [VOGRE-36] Merge migrations

--- annotations/static/annotations/js/annotators/text.js
+++ annotations/static/annotations/js/annotators/text.js
@@ -205,7 +205,11 @@ ConceptCreator = {
             pos: "",
             concept_types: [],
             error: false,
-            submitted: false
+            submitted: false,
+            user: {
+                id: USER_ID,
+                username: USER_NAME
+            },
         }
     },
     mounted: function () {
@@ -251,7 +255,8 @@ ConceptCreator = {
                     label: this.name,
                     description: this.description,
                     pos: this.pos,
-                    typed: this.concept_type
+                    typed: this.concept_type,
+                    createdBy: this.user.id
                 }).then(function (response) {
                     self.clear();
                     self.$emit("createdconcept", response.body);
