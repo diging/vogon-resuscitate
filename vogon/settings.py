333a7a17567ba571b50a0d343239ade3b83c54a6
Rajvi Patel <rajvi2232000@gmail.com>
2025-04-29 14:28:22 -0700

    [VOGRE-31] Upgrade packages to support django 4+

--- vogon/settings.py
+++ vogon/settings.py
@@ -14,8 +14,16 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 from urllib.parse  import urlparse
 from dotenv import load_dotenv
+import django
+from django.utils.encoding import smart_str
+from django.utils.encoding import force_str
+from django.utils.translation import gettext_lazy
 
 load_dotenv()
+django.utils.encoding.smart_text = smart_str
+django.utils.encoding.force_text = force_str
+django.utils.translation.ugettext_lazy = gettext_lazy
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -242,7 +250,7 @@ LOGIN_URL = f'/{APP_ROOT}login/'
 LOGOUT_URL = f'/{APP_ROOT}logout/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
-ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
 ACCOUNT_UNIQUE_EMAIL = True
 
 BASE_URL = os.path.join(os.getenv('BASE_URL', '/'), APP_ROOT)
@@ -264,3 +272,6 @@ CITESPHERE_ITEM_PAGE = 50
 VOGON_ADMIN_PAGE_SIZE = 15
 REPOSITORY_TEXT_PAGINATION_PAGE_SIZE = 20
 PROJECT_TEXT_PAGINATION_PAGE_SIZE = 20
+
+# Django 3.2+ requires to explicitly define the type of default auto-created primary keys.
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
