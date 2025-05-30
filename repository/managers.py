082644fea4869fa0ecc5635c585530c13b590e30
Rajvi Patel <rajvi2232000@gmail.com>
2025-05-30 16:23:34 -0700

    [VOGRE-75] Remove extra space

--- repository/managers.py
+++ repository/managers.py
@@ -189,7 +189,7 @@ class RepositoryManager:
         headers = auth.citesphere_auth(self.user, self.repository)
         url = f"{self.repository.endpoint}/api/v1/groups/{groupId}/items/{itemId}/"
         response = requests.get(url, headers=headers)
-        
+
         if response.status_code == 200:
             item_data = response.json()
 
