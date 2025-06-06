091b6c9d5fb677e524ddcf591a142a9b6571762d
Girik1105 <girik1105@gmail.com>
2025-06-06 12:24:26 -0700

    [VOGRE-61] Failing tests in github fix

--- vogon/settings.py
+++ vogon/settings.py
@@ -108,8 +108,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': '5432',
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
         'OPTIONS': {
             'options': '-c timezone=UTC',
         },
